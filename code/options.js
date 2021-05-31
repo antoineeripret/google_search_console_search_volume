0212bde19650e2e3239a556e6d0c3af2d160cbcf
Antoine Eripret <antoine.eripret@gmail.com>
2021-05-31 19:40:22 +0200

    final update for mvp

--- code/options.js
+++ code/options.js
@@ -1,28 +1,22 @@
+const optionsStatus = document.getElementById('status');
+const countryList = document.getElementById('country');
+const optionsSaveBtn = document.getElementById('save');
+
+
 // Saves options to chrome.storage
 function save_options() {
-    var country = document.getElementById('country').value;
-    chrome.storage.local.set({
-        'country': country
-    }, function() {
-      // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
-    });
-  }
-  
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
-  function restore_options() {
+    var country = countryList.value;
+    localStorage.setItem('country', country); 
+    optionsStatus.textContent = 'Option saved.';
+    setTimeout(function() {status.textContent = '';}, 750);
+};
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
     // Use default value color = 'red' and likesColor = true.
-    chrome.storage.local.get({
-      'country': 'fr',
-    }, function(items) {
-      document.getElementById('country').value = items.country;
-    });
-  }
-  document.addEventListener('DOMContentLoaded', restore_options);
-  document.getElementById('save').addEventListener('click',
-      save_options);
+        countryList.value = localStorage['country'];
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+optionsSaveBtn.addEventListener('click', save_options);
